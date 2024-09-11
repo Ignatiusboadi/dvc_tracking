a0b7722f436225d0de7dc0f833931aedb32b4e4e
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-09-11 00:15:03 +0000

    imported required modules; set random_state

---
+++ mlflow_practice.py
@@ -0,0 +1,16 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+
+from mlflow.models.signature import infer_signature
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+random_state = 50
+
+
+
+
+
